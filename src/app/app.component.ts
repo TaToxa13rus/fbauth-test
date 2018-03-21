81dfb53ce79ff97435c63c719adb95c91a466b61
tabachkov <tabachkovav@gmail.com>
2018-03-21 19:22:09 +0300

    complete google auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { AuthService } from './providers/auth.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  private isLoggedIn: Boolean;
+  private user_displayName: String;
+  private user_email: String;
+  constructor(public authService: AuthService, private router: Router) {
+    this.authService.af.authState.subscribe(
+      (auth) => {
+        if (auth == null) {
+          console.log('Logged out', auth);
+          this.isLoggedIn = false;
+          this.user_displayName = '';
+          this.user_email = '';
+          this.router.navigate(['login']);
+        } else {
+          this.isLoggedIn = true;
+          this.user_displayName = auth.displayName;
+          this.user_email = auth.email;
+          console.log('Logged in');
+          console.log(auth);
+          this.router.navigate(['']);
+        }
+      }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
+// for AngularFireAuth
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AuthService } from './providers/auth.service';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
+export const firebaseConfig = {
+    apiKey: 'AIzaSyBlplc8xQzPs8EOxTrLg6kTJ4ZK_ZUImwY',
+    authDomain: 'ngfbauth-f981a.firebaseapp.com',
+    databaseURL: 'https://ngfbauth-f981a.firebaseio.com',
+    projectId: 'ngfbauth-f981a',
+    storageBucket: 'ngfbauth-f981a.appspot.com',
+    messagingSenderId: '825631089346'
+};
+
+const routes: Routes = [
+    { path: '', component: HomePageComponent },
+    { path: 'login', component: LoginPageComponent }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LoginPageComponent,
+        HomePageComponent
+    ],
+    imports: [
+        BrowserModule,
+        AngularFireModule.initializeApp(firebaseConfig),
+        AngularFireAuthModule,
+        RouterModule.forRoot(routes)
+    ],
+    providers: [AuthService],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
